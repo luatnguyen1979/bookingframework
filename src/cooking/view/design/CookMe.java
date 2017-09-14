1069a9579caa1562a6d0eff67da59359ed2d9652
RamyBadawy <31967549+RamyBadawy@users.noreply.github.com>
2017-09-14 18:46:04 -0500

    Nothing modified here

--- src/cooking/view/design/CookMe.java
+++ src/cooking/view/design/CookMe.java
@@ -32,7 +32,7 @@ public class CookMe extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(args);
     	
     	
     }
