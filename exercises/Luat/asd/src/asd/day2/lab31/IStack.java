8cf402379f696f8e3829dc8218645014928e6af8
luatnguyen1979 <vglnguyen@mum.edu>
2018-01-16 22:58:53 -0600

    Add resource exams

---
+++ exercises/Luat/asd/src/asd/day2/lab31/IStack.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package asd.day2.lab31;
+
+/**
+ * @author luatnguyen
+ *
+ */
+public interface IStack <T> {
+	/**
+	 * Returns the object at the top of the Stack without removing it.
+	 * 
+	 */
+	public T peek();
+	
+	/**
+	 * 
+	 * Removes and returns the object at the top of the Stack.
+	 */
+	public T pop();
+	
+	/**
+	 * 	Inserts an object at the top of the Stack.
+	 */
+	public void push(T obj);
+	
+}
