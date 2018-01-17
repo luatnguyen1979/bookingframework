c5b40a7e79a448df6e4f456313926588d57a006b
eegii <eegii332002@yahoo.com>
2018-01-17 14:08:03 -0600

    I added my project.

---
+++ exercises/Enkh/Laborator/src/main/java/usa/edu/mum/asd/labs/lab1/Singleton.java
@@ -0,0 +1,8 @@
+package usa.edu.mum.asd.labs.lab1;
+
+/**
+ * 
+ */
+public class Singleton {
+
+}
---
+++ exercises/Enkh/Laborator/src/main/java/usa/edu/mum/asd/labs/lab2/adapter/Target2.java
@@ -0,0 +1,12 @@
+package usa.edu.mum.asd.labs.lab2.adapter;
+
+public interface Target2 {
+
+    public void enqueue(String str);
+
+    public String dequeue();
+
+    public boolean isEmpty();
+
+    public int size();
+}
