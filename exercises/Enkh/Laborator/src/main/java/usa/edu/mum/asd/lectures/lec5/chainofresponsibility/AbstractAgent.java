bfe79bdcea50c31d4e773e09222026199273594b
eegii <eegii332002@yahoo.com>
2018-01-19 14:18:52 -0600

    Test

--- exercises/Enkh/Laborator/src/main/java/usa/edu/mum/asd/lectures/lec5/chainofresponsibility/AbstractAgent.java
+++ exercises/Enkh/Laborator/src/main/java/usa/edu/mum/asd/lectures/lec5/chainofresponsibility/AbstractAgent.java
@@ -1,5 +1,11 @@
 package usa.edu.mum.asd.lectures.lec5.chainofresponsibility;
 
-public class AbstractAgent {
+public abstract class AbstractAgent {
 
+    protected AbstractAgent nextAgent;
+
+    abstract public void handleRequest(Request req);
+
+    boolean canHandleRequest(Request req) {
+    }
 }
