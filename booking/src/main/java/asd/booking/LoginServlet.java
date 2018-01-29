0ee39221f266bba4a2c66ab69924ea3937a1d3b0
luatnguyen1979 <vglnguyen@mum.edu>
2018-01-29 12:09:18 -0600

    update the project

---
+++ booking/src/main/java/asd/booking/LoginServlet.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package asd.booking;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import asd.booking.dao.UserDAO;
+import asd.booking.domain.UserBean;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+public class LoginServlet extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6645193318867169686L;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, java.io.IOException {
+
+		try {
+
+			UserBean user = new UserBean();
+			user.setUserName(request.getParameter("un"));
+			user.setPassword(request.getParameter("pw"));
+
+			user = UserDAO.login(user);
+
+			if (user.isValid()) {
+
+				HttpSession session = request.getSession(true);
+				session.setAttribute("currentSessionUser", user);
+				response.sendRedirect("userLogged.jsp"); // logged-in page
+			}
+
+			else
+				response.sendRedirect("invalidLogin.jsp"); // error page
+		}
+
+		catch (Throwable theException) {
+			System.out.println(theException);
+		}
+	}
+}	
