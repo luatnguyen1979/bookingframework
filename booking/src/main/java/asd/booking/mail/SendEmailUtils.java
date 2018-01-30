dcab5794514717cb89cc73d0fccf21abdbcc92e3
Kimtey CHAV <kimtey.chav@gmail.com>
2018-01-29 21:14:47 -0600

    send email

--- booking/src/main/java/asd/booking/mail/SendEmailUtils.java
+++ booking/src/main/java/asd/booking/mail/SendEmailUtils.java
@@ -18,6 +18,8 @@ import asd.booking.domain.User;
 public class SendEmailUtils {
 
     private static SendEmailUtils INSTNANCE;
+    private final String USERNAME = "chav.vecteur@gmail.com"; //user send
+    private final String PASSWORD = "012249021"; //passwordfdfdffd
 
     private SendEmailUtils() {
 
@@ -29,40 +31,36 @@ public class SendEmailUtils {
         return INSTNANCE;
     }
 
-    /**
-     * 
-     * @param user
-     */
-    public void sendEmail(User user) {
-        final String username = "chav.vecteur@gmail.com"; //user send
-        final String password = "012249021"; //password
-
+    private Properties getPropertiesMail() {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
-
+        return props;
+    }
+    
+    /**
+     * 
+     * @param user
+     */
+    public void sendEmail(User user) {
+        
+        Properties props = getPropertiesMail();
         Session session = Session.getInstance(props,
           new javax.mail.Authenticator() {
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication(username, password);
+                return new PasswordAuthentication(USERNAME, PASSWORD);
             }
           });
-
         try {
 
             Message message = new MimeMessage(session);
-//            message.setFrom(new InternetAddress("booking@gmail.com"));
-            message.setRecipients(Message.RecipientType.TO,
-                InternetAddress.parse("kimtey.chav@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("kimtey.chav@gmail.com"));
             message.setSubject("Testing Subject");
             message.setText("Testing body test");
-
             Transport.send(message);
-
             System.out.println("Done");
-
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
