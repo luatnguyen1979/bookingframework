3d9cec0daf640fbe819ca48f635ae0575ea39018
Kimtey CHAV <kimtey.chav@gmail.com>
2018-01-30 23:31:07 -0600

    update traindao

--- booking/src/main/java/asd/booking/mail/Main.java
+++ booking/src/main/java/asd/booking/mail/Main.java
@@ -3,11 +3,19 @@
  */
 package asd.booking.mail;
 
+import asd.booking.dao.TrainDAO;
+import asd.booking.dao.UserDAO;
+import asd.booking.domain.User;
 
 public class Main {
 
     public static void main(String[] args) {
-        SendEmailUtils.getInstance().sendEmail(null);
+//        SendEmailUtils.getInstance().sendEmail(null);
+        User user = new User();
+        user.setUserName("kimtey");
+        user.setPassword("123");
+        UserDAO.login(user);
+        TrainDAO.findByFromAndTo("Chicago", "Gary");
     }
 
 }
