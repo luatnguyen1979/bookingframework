69a1b3ba3c42ed29da3c24f3c06a5384bffb7cb6
eegii <eegii332002@yahoo.com>
2018-02-01 10:55:44 -0600

    PortDAO is refactored. Added day4 status

--- booking/src/main/java/asd/booking/dao/PortDAO.java
+++ booking/src/main/java/asd/booking/dao/PortDAO.java
@@ -6,116 +6,107 @@ import asd.booking.domain.trip.Port;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 
 public class PortDAO {
-	static Connection currentCon = null;
-	static ResultSet rs = null;
+    static Connection currentCon = null;
+    static ResultSet rs = null;
+    static PreparedStatement ps = null;
 
-	public static List<Port> getList() {
-		List<Port> ret = new LinkedList<>();
-		final String sql = "SELECT * FROM port";
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			ps = Database.getInstance().preparedStatement(sql);
-			rs = ps.executeQuery();
-			while (rs.next()) {
-				Address address = AddressDAO.get(rs.getInt("address_id"));
-				Port port = new Port(rs.getInt("id"), rs.getString("name"), address);
-				ret.add(port);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			Database.getInstance().close(ps);
-			Database.getInstance().close(rs);
-		}
-		return ret;
-	}
+    public static List<Port> getPortList() {
+        List<Port> portList = new ArrayList<Port>();
 
-	public static List<Port> getPortList() {
-		List<Port> portList = new ArrayList<Port>();
-		Statement stmt = null;
+        final String searchQuery = "SELECT * FROM PORT";
+        // process
 
-		final String searchQuery = "SELECT * FROM PORT";
-		// process
+        System.out.println("Query: " + searchQuery);
 
-		System.out.println("Query: " + searchQuery);
+        try {
+            // connect to DB
+            currentCon = ConnectionManager.getConnection();
+            ps = currentCon.prepareStatement(searchQuery);
+            rs = ps.executeQuery(searchQuery.toString());
+            while (rs.next()) {
+                int addressId = rs.getInt("address_id");
+                Address address = AddressDAO.get(addressId);
+                Port port = new Port(rs.getInt("id"), rs.getString("name"), address);
+                portList.add(port);
+            }
 
-		try {
-			// connect to DB
-			currentCon = ConnectionManager.getConnection();
-			stmt = currentCon.createStatement();
-			rs = stmt.executeQuery(searchQuery.toString());
-			while (rs.next()) {
-				int addressId = rs.getInt("address_id");
-				Address address = AddressDAO.get(addressId);
-				Port port = new Port(rs.getInt("id"), rs.getString("name"), address);
-				portList.add(port);
-			}
+            System.out.println("Port list size: " + portList.size());
 
-			System.out.println("Port list size: " + portList.size());
+        } catch (Exception ex) {
+            System.out.println("Log In failed: An Exception has occurred! " + ex);
+        }
 
-		}
+        // some exception handling
+        finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                }
+                rs = null;
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (Exception e) {
+                }
+                ps = null;
+            }
+            if (currentCon != null) {
+                try {
+                    currentCon.close();
+                } catch (Exception e) {
+                }
 
-		catch (Exception ex) {
-			System.out.println("Log In failed: An Exception has occurred! " + ex);
-		}
+                currentCon = null;
+            }
+        }
 
-		// some exception handling
-		finally {
-			if (rs != null) {
-				try {
-					rs.close();
-				} catch (Exception e) {
-				}
-				rs = null;
-			}
+        return portList;
+    }
 
-			if (stmt != null) {
-				try {
-					stmt.close();
-				} catch (Exception e) {
-				}
-				stmt = null;
-			}
+    public static Port get(int id) {
+        Port ret = null;
+        final String sql = "SELECT * FROM port WHERE id = ?";
+        try {
+            currentCon = ConnectionManager.getConnection();
+            ps = currentCon.prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                Address address = AddressDAO.get(rs.getInt("address_id"));
+                ret = new Port(rs.getInt("id"), rs.getString("name"), address);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (Exception e) {
+                }
+                rs = null;
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (Exception e) {
+                }
+                ps = null;
+            }
+            if (currentCon != null) {
+                try {
+                    currentCon.close();
+                } catch (Exception e) {
+                }
 
-			if (currentCon != null) {
-				try {
-					currentCon.close();
-				} catch (Exception e) {
-				}
-
-				currentCon = null;
-			}
-		}
-
-		return portList;
-	}
-
-	public static Port get(int id) {
-		Port ret = null;
-		final String sql = "SELECT * FROM port WHERE id = ?";
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			ps = Database.getInstance().preparedStatement(sql, id);
-			rs = ps.executeQuery();
-			if (rs.next()) {
-				Address address = AddressDAO.get(rs.getInt("address_id"));
-				ret = new Port(rs.getInt("id"), rs.getString("name"), address);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			Database.getInstance().close(ps);
-			Database.getInstance().close(rs);
-		}
-		return ret;
-	}
+                currentCon = null;
+            }
+        }
+        return ret;
+    }
 }
---
+++ booking/src/main/test/asd/booking/dao/PassengerDAOTest.java
@@ -0,0 +1,12 @@
+package asd.booking.dao;
+
+import junit.framework.TestCase;
+
+public class PassengerDAOTest extends TestCase {
+
+    public void testInsert() {
+    }
+
+    public void testGetList() {
+    }
+}
